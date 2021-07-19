74bfa72cb4cb86333dd5256637e4c3f85c4a8927
devunderslash <devunderslash@hotmail.com>
2021-07-19 14:00:10 +0100

    updating merge script on oldest branch

--- auto-merge.sh
+++ auto-merge.sh
@@ -5,22 +5,29 @@
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 LAST_COMMIT=$(git rev-list -1 HEAD)
 
-echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to master
+echo Automatically merging commit $LAST_COMMIT from $CURRENT_BRANCH rippling to main
 
 case $CURRENT_BRANCH in
-2.1)
-  git checkout release-1 ＆＆ git merge $CURRENT_BRANCH
-  git checkout release-2 ＆＆ git merge release-1
-  git checkout main ＆＆ git merge release-2
+
+release-1)
+  git checkout release-1
+  git merge $CURRENT_BRANCH
+
+  git checkout release-2 
+  git merge release-1
+
+  git checkout main 
+  git merge release-2
+
   git checkout $CURRENT_BRANCH
   ;;
-2.2)
-  git checkout release-2 ＆＆ git merge release-1
-  git checkout main ＆＆ git merge release-2
-  git checkout $CURRENT_BRANCH
-  ;;
-2.3)
-  git checkout main ＆＆ git merge release-2
+release-2)
+  git checkout release-2 
+  git merge release-1
+
+  git checkout main
+  git merge release-2
+  
   git checkout $CURRENT_BRANCH
   ;;
 release-3)
