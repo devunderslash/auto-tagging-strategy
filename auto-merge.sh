e1f5634482aa9c0cbd8d888d0fc5e7540062e44f
devunderslash <devunderslash@hotmail.com>
2021-07-19 14:42:08 +0100

    incremental update to include rel-3

--- auto-merge.sh
+++ auto-merge.sh
@@ -16,8 +16,11 @@ release-1)
   git checkout release-2 
   git merge release-1
 
-  git checkout main 
+  git checkout release-3 
   git merge release-2
+
+  git checkout main 
+  git merge release-3
 
   git checkout $CURRENT_BRANCH
   ;;
